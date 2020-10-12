5c6bf650d03d2ec5f1699fce1d414e76bf695863
Spiritusanti <mccrackenjacob@gmail.com>
2020-10-12 10:17:16 -0700

    added search functionality

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [] 
+      monsters: [],
+      searchField: '' 
   }
 }
 
@@ -23,9 +24,19 @@ componentDidMount() {
 
 // the function that puts our app on the page
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+     )
     return (
     <div className="App">
-    <CardList monsters={this.state.monsters}/>
+      <input 
+        type='search' 
+        placeholder='search monsters' 
+        onChange={e => {
+          this.setState({searchField: e.target.value}
+          )}} 
+          />
+      <CardList monsters={filteredMonsters}/>
   </div>)
   }
 }
