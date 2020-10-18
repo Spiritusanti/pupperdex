d8d664260775a94925c1309011c1802d7b99f580
Spiritusanti <mccrackenjacob@gmail.com>
2020-10-18 16:54:49 -0700

    Updates

---
+++ precache-manifest.eb44f7bc04962d0741b29701014eb368.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6e62c39f098dbb1a1abe838b80b4c9",
+    "url": "/pupperdex/index.html"
+  },
+  {
+    "revision": "a557a64f6e7e9949eddf",
+    "url": "/pupperdex/static/css/main.f0ef318e.chunk.css"
+  },
+  {
+    "revision": "41482f722562ff2dd080",
+    "url": "/pupperdex/static/js/2.b0e162e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pupperdex/static/js/2.b0e162e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a557a64f6e7e9949eddf",
+    "url": "/pupperdex/static/js/main.99a37a74.chunk.js"
+  },
+  {
+    "revision": "5b2e94d737dc04664373",
+    "url": "/pupperdex/static/js/runtime-main.84a8810f.js"
+  }
+]);
